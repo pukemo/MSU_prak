b322ac50f8bd88ef9f6954c73151d8bd929d7b93
pukemo <71704642+pukemo@users.noreply.github.com>
2020-12-08 16:49:39 +0300

    Update task1.c

--- task1.c
+++ task1.c
@@ -7,24 +7,30 @@
 int pipedes[2];
 
 int main (unsigned int argc, char* argv[]) {
+    
     pid_t pid1, pid2;
-    if (argc < 3) {
+    if (argc < 2) {
         return 1;
     }
+    
     pipe(pipedes);
     if (!(pid1 = fork())) {
         dup2(pipedes[1], 1);
         close(pipedes[0]);
         execl("bin/ls", "ls", argv[1], NULL);
         exit(1);
-    } 
+    }
     if (!(pid2 = fork())) {
         dup2(pipedes[0], 0);
-        execl("bin/wc", "wc",)
+        execl("bin/wc", "wc", "-w", NULL);
+        exit(1);
     }
-
+    
     close (pipedes[0]);
     close (pipedes[1]);
-
+    
+    wait(-1);
+    wait(-1);
+    
     return 0;
 }
