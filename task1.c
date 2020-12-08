547f614ed828427b558b823662bf3533775c377c
pukemo <71704642+pukemo@users.noreply.github.com>
2020-12-08 15:43:19 +0300

    Update task1.c

--- task1.c
+++ task1.c
@@ -8,9 +8,9 @@
 
 int main(int argc, char* argv[]) {
     if(argc < 2) {return(0);}
-    int fd = open("tmp.txt", O_RDWR|O_CREAT|O_TRUNC, 0600);
-    execlp(argv[1], "ls", ">", "tmp.txt", NULL);
-    execlp("tmp.txt", "wc", "-w", NULL);
+    int fd = open("tmp.txt", O_RDWR|O_CREAT|O_TRUNC, 0600); 
+    system("ls -l > tmp.txt"); 
+    system("wc -w tmp.txt")
     remove("tmp.txt");
     return 0;
 }
